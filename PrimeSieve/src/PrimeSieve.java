446c6104efe0c5da1a28b64fbb53cf86f0533905
Stefan Wenzlhumer <stefan.wenzlhumer@gmail.com>
2024-09-17 12:00:03 +0200

    implement interface

---
+++ PrimeSieve/src/PrimeSieve.java
@@ -0,0 +1,7 @@
+public interface PrimeSieve {
+    //Ueberprueft ob p eine Primzahl ist oder nicht.
+    public boolean isPrime(int p);
+
+    //Gibt alle Primzahlen bis zur Obergrenze auf der Konsoleaus.
+    public void printPrimes();
+}
