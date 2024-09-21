1aee189646952eda3c9ff933c6a999cf0607f83b
Stefan Wenzlhumer <stefan.wenzlhumer@gmail.com>
2024-09-21 14:54:43 +0200

    finish

--- PrimeSieve/src/EratosthenesPrimeSieve.java
+++ PrimeSieve/src/EratosthenesPrimeSieve.java
@@ -1,27 +1,33 @@
 public class EratosthenesPrimeSieve implements PrimeSieveI {
 
     public static int upperLimit = 500;
-    public static boolean[] primeArray = new boolean[upperLimit + 1];
+    public static boolean[] primeArray = new boolean[upperLimit + 1]; // false = prime, true = non-prime
 
 
-    public static boolean isPrime(int p) {
+    public static void sieve() {
+
+        primeArray[0] = true;
+        primeArray[1] = true;
+
         for (int i = 2; i <= Math.sqrt(upperLimit); i++) {
             if (!primeArray[i]) {
-
                 for (int j = i * i; j <= upperLimit; j += i) {
                     primeArray[j] = true;
-                    return true;
                 }
-
             }
-
         }
+    }
 
-        return false;
+    public static boolean isPrime(int p) {
+        if (p <= 1) {
+            return false;
+        }
+        return !primeArray[p];
     }
 
 
     public static void printPrimes() {
+        sieve();
         for (int i = 2; i <= upperLimit; i++) {
             if (!primeArray[i]) {
                 System.out.print(i + " ");
--- PrimeSieve/src/PrimeSieveI.java
+++ PrimeSieve/src/PrimeSieveI.java
@@ -1,10 +1,8 @@
 public interface PrimeSieveI {
-    //Ueberprueft ob p eine Primzahl ist oder nicht.
     public static boolean isPrime(int p) {
         return false;
     }
 
-    //Gibt alle Primzahlen bis zur Obergrenze auf der Konsoleaus.
     public static void printPrimes() {
 
     }
